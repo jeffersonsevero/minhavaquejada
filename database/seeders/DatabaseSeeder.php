c6bfb8b20d925b4d53d070f77f66003373232d11
Jeff <jefferson.severo@refuturiza.com.br>
2026-01-05 18:09:43 -0300

    wip

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +20,8 @@ class DatabaseSeeder extends Seeder
             PassSeeder::class,
             // CowboySeeder::class,
         ]);
+
+        User::factory(10)->create();
+
     }
 }
